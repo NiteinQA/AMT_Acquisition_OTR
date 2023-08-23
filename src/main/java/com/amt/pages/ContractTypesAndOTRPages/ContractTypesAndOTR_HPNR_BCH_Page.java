4842032d3e746eeb136c06074904a9b561cf73b0
Nitein A. Kurane <nitein.kurane@gatewaygroup.com>
2023-08-23 20:08:14 +0530

    23.08.2023

--- src/main/java/com/amt/pages/ContractTypesAndOTRPages/ContractTypesAndOTR_HPNR_BCH_Page.java
+++ src/main/java/com/amt/pages/ContractTypesAndOTRPages/ContractTypesAndOTR_HPNR_BCH_Page.java
@@ -160,16 +160,8 @@ public class ContractTypesAndOTR_HPNR_BCH_Page extends TestBase {
 		ExplicitWait.waitTillLoadingIconDisappears(driver, loading_icon, 30);
 
 		ExplicitWait.visibleElement(driver, acq_contractTypes_table_calculation_basic_vehicle_price, 30);
-		acq_contractTypes_table_calculation_basic_vehicle_price.click();
-
-		acq_contractTypes_table_calculation_basic_vehicle_price.sendKeys(Keys.chord(Keys.CONTROL, "a", "c"));
-
-		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-		String vehicle_price_copied = (String) clipboard.getData(DataFlavor.stringFlavor);
-		
-//		StringSelection stringSelection = new StringSelection("");
-//		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
-
+		String vehicle_price_copied = acq_contractTypes_table_calculation_basic_vehicle_price.getAttribute("value");           
+	  
 		obj_read_excel_calculation_page = new ReadExcelCalculation();
 
 		double subtotal_after_discount_excel = obj_read_excel_calculation_page
